c684452e9f9a6c81eb864102aad9134bf9acb2c0
Pure-Peace <940857703@qq.com>
2022-07-05 11:23:28 +0800

    Fix tests

--- test/MelosDao.test.ts
+++ test/MelosDao.test.ts
@@ -7,7 +7,7 @@ import {
 
 import { getBlockNumber, setupUsersWithNames } from './utils';
 import { getContractForEnvironment } from './utils/getContractForEnvironment';
-import { MelosGovernorV1, TestMelos, VoteMelos } from '../typechain';
+import { MelosGovernorV1, MockMelos, VMelos } from '../typechain';
 import { BigNumber } from 'ethers';
 import { toTokenAmount } from '../scripts/utils';
 
@@ -16,9 +16,9 @@ const UINT256_MAX = BigNumber.from(2).pow(256).sub(1)
 const setup = deployments.createFixture(async () => {
   await deployments.fixture('deployContracts');
   const contracts = {
-    TestMelos: await getContractForEnvironment<TestMelos>(hre, 'TestMelos'),
+    MockMelos: await getContractForEnvironment<MockMelos>(hre, 'MockMelos'),
     MelosGovernorV1: await getContractForEnvironment<MelosGovernorV1>(hre, 'MelosGovernorV1'),
-    VoteMelos: await getContractForEnvironment<VoteMelos>(hre, 'VoteMelos'),
+    VoteMelos: await getContractForEnvironment<VMelos>(hre, 'vMelos'),
   };
   const users = await setupUsersWithNames((await getNamedAccounts()) as any, contracts);
   return {
@@ -30,26 +30,26 @@ const setup = deployments.createFixture(async () => {
 
 describe('TEST VOTE MELOS', function () {
   it('test Melos should approve for vMelos', async function () {
-    const { users, TestMelos, VoteMelos } = await setup();
-    await TestMelos.approve(VoteMelos.address, UINT256_MAX)
-    expect(await TestMelos.allowance(users.deployer.address, VoteMelos.address), 'approve failed').to.equal(UINT256_MAX)
+    const { users, MockMelos, VoteMelos } = await setup();
+    await MockMelos.approve(VoteMelos.address, UINT256_MAX)
+    expect(await MockMelos.allowance(users.deployer.address, VoteMelos.address), 'approve failed').to.equal(UINT256_MAX)
   });
 
   it('should deposit Melos for vMelos success', async function () {
-    const { users, TestMelos, VoteMelos } = await setup();
+    const { users, MockMelos, VoteMelos } = await setup();
     const depositValue = toTokenAmount(10000)
 
-    await TestMelos.approve(VoteMelos.address, UINT256_MAX)
+    await MockMelos.approve(VoteMelos.address, UINT256_MAX)
     await VoteMelos.depositFor(users.deployer.address, depositValue)
 
     expect(await VoteMelos.balanceOf(users.deployer.address), 'deposit failed').to.equal(depositValue)
   });
 
   it('should withdraw Melos for vMelos success', async function () {
-    const { users, TestMelos, VoteMelos } = await setup();
+    const { users, MockMelos, VoteMelos } = await setup();
     const depositValue = toTokenAmount(10000)
 
-    await TestMelos.approve(VoteMelos.address, UINT256_MAX)
+    await MockMelos.approve(VoteMelos.address, UINT256_MAX)
     await VoteMelos.depositFor(users.deployer.address, depositValue)
     await VoteMelos.withdrawTo(users.user1.address, depositValue)
 
@@ -57,9 +57,9 @@ describe('TEST VOTE MELOS', function () {
   });
 
   it('deposit testing', async function () {
-    const { users, TestMelos, VoteMelos } = await setup();
+    const { users, MockMelos, VoteMelos } = await setup();
 
-    await TestMelos.approve(VoteMelos.address, UINT256_MAX)
+    await MockMelos.approve(VoteMelos.address, UINT256_MAX)
 
     await VoteMelos.depositFor(users.deployer.address, toTokenAmount(1000))
     await VoteMelos.depositFor(users.deployer.address, toTokenAmount(100))
